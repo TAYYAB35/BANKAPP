c468785f5d67760dbe10d63f2bd910df64f58083
TAYYAB35 <tayyabfmtech@gmail.com>
2025-02-07 19:06:51 +0500

    reset password add

--- src/app/pages/Layout/settingScreen/setting.component.ts
+++ src/app/pages/Layout/settingScreen/setting.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './setting.component.html',
   styles: `
   .card{
@@ -13,6 +14,34 @@ import { Component } from '@angular/core';
   `
 })
 export class SettingComponent {
+  showStep1: boolean = false; // Pehla step dikhane ke liye
+  showStep2: boolean = false;
+
+  otp1: string = '';
+  otp2: string = '';
+  otp3: string = '';
+  otp4: string = '';
+  otp5: string = '';
+
+  // Jab edit button click ho, pehla step show ho
+  showFirstStep() {
+    this.showStep1 = true;
+  }
+
+  // OTP enter hone ke baad second step show karna
+  moveToNext(event: Event, nextField: HTMLInputElement | null) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.value.length === 1 && nextField) {
+      nextField.focus();
+    }
+
+    // Jab 5 OTP boxes fill ho jayein, dusra step show karein
+    if (this.otp1 && this.otp2 && this.otp3 && this.otp4 && this.otp5) {
+      this.showStep2 = true;
+    }
+  }
+
   profile = {
     name: 'Mohamed M. Raouf',
     email: 'Mohamed.M.Raouf@Gmail.Com',
