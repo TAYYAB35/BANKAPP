e4d29458757dd18de9c7d672b828a4300929b296
TAYYAB35 <tayyabfmtech@gmail.com>
2025-02-10 18:51:35 +0500

    details add

--- src/app/components/payment-details/payment-details.component.ts
+++ src/app/components/payment-details/payment-details.component.ts
@@ -10,6 +10,17 @@ import { RouterLink } from '@angular/router';
   styles: ``
 })
 export class PaymentDetailsComponent {
-  paymentDetails: any;
+  paymentDetails = {
+    number: '+2491234567',
+    account: '66682375877898968',
+    amount: 'SDG 32,000,000',
+    date: '22.3.2024',
+    services: 'MTN Bill Inquiry',
+    transactionId: '24354657869788765'
+  };
 
+  downloadInvoice() {
+    // Logic to generate/download the invoice (placeholder function)
+    console.log('Downloading Invoice...');
+  }
 }
